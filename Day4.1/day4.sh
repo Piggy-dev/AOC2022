a3c28107cedeb3bc8c46d97a4174ba1a28340474
Piggy-dev <thomas_pigg@hotmail.com>
2022-12-06 19:33:41 -0500

    Day 4 Part 1

---
+++ Day4.1/day4.sh
@@ -0,0 +1,28 @@
+#!/bin/ksh
+total=0
+for i in $(cat input.txt)
+do 
+    match=0
+    match=0
+    a="$(cut -d',' -f1 <<<"$i")"
+    b="$(cut -d',' -f2 <<<"$i")"
+    a1="$(cut -d'-' -f1 <<<"$a")"
+    a2="$(cut -d'-' -f2 <<<"$a")"
+    b1="$(cut -d'-' -f1 <<<"$b")"
+    b2="$(cut -d'-' -f2 <<<"$b")"
+
+
+    if [[ $a1 -le $b1 ]] && [[ $a2 -ge $b2 ]];
+    then
+        total=$((total+1))
+        match=1
+    fi
+    if [[ $match == 0 ]];
+    then
+        if [[ $b1 -le $a1 ]] && [[ $b2 -ge $a2 ]];
+        then
+            total=$((total+1))
+        fi
+    fi
+done
+echo $total
