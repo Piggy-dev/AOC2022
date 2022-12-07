3fae212612759fc4033c21e568d33de11b5dcbb7
Piggy-dev <thomas_pigg@hotmail.com>
2022-12-06 19:45:06 -0500

    Cleanup

--- Day4.1/day4.sh
+++ Day4.1/day4.sh
@@ -3,14 +3,12 @@ total=0
 for i in $(cat input.txt)
 do 
     match=0
-    match=0
     a="$(cut -d',' -f1 <<<"$i")"
     b="$(cut -d',' -f2 <<<"$i")"
     a1="$(cut -d'-' -f1 <<<"$a")"
     a2="$(cut -d'-' -f2 <<<"$a")"
     b1="$(cut -d'-' -f1 <<<"$b")"
     b2="$(cut -d'-' -f2 <<<"$b")"
-
 
     if [[ $a1 -le $b1 ]] && [[ $a2 -ge $b2 ]];
     then
